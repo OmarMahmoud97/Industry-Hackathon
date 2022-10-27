bcaedb6e92bdebba693b1e6d18e00e5fac3ff25f
Rolphy Lu Morrell <rlumorrell19@gmail.com>
2022-10-27 22:41:56 +0100

    testimonial section

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import "./styles/partials/_resets.scss";
 import Navbar from "./components/Navbar/navbar";
 import Landing from "./components/Landing/landing";
 import Footer from "./components/Footer/footer";
+import Testimonial from "./components/Testimonial/testimonial";
+
 function App() {
   return (
     <div className="App">
       <Landing />
+      <Testimonial />
       <Footer />
     </div>
   );
--- src/components/Testimonial/testimonial.jsx
+++ src/components/Testimonial/testimonial.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const testimonial = () => {
-  return <div>testimonial</div>;
+  return (
+    <section className="testimonial">
+      <div className="testmonial__subtitle">
+        “Lorem ipsum dolor sit amet, consectetur adipis cing elit. Integer
+        gravida”
+      </div>
+      <div className="testimonial__title">
+        The EU needs to hold the financial sector in. The EU needs to hold the
+        financial sector in global deforestation. The EU needs to hold the
+        financial sector in global{" "}
+      </div>
+      <div className="testimonial__star-rating"></div>
+      <div className="testimonial__quote">“Lorem ipsum dolor.”</div>
+    </section>
+  );
 };
 
 export default testimonial;
