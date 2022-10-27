e2b439b4b0f49b65d43fbd6c9b74c6e415902d6a
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-10-27 21:23:36 +0100

    add stats page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles/partials/_resets.scss";
 import Navbar from "./components/Navbar/navbar";
 import Landing from "./components/Landing/landing";
+import PageTwo from "./components/PageTwo/pageTwo";
 function App() {
   return (
     <div className="App">
       <Landing />
+      <PageTwo />
     </div>
   );
 }
---
+++ src/components/PageTwo/pageTwo.jsx
@@ -0,0 +1,24 @@
+import "./pageTwo.scss";
+const PageTwo = () => {
+  return (
+    <section className="pageTwo">
+      <div className="pageTwo__header">
+        <h2 className="pageTwo__header-text">
+          The EU needs to hold the financial sector in.
+        </h2>
+      </div>
+      <div className="pageTwo__subheader">
+        <h3 className="pageTwo__subheader-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          velit! Lorem ipsum dolor sit amet.
+        </h3>
+      </div>
+      <div className="pageTwo__stats">
+        <div className="pageTwo__stats-item">item</div>
+        <div className="pageTwo__stats-item">item</div>
+        <div className="pageTwo__stats-item">item</div>
+      </div>
+    </section>
+  );
+};
+export default PageTwo;
