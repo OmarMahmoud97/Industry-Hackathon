4d6465e019ce1691d0b0be27c4b3e705eef8d577
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-10-27 22:34:28 +0100

    add phone section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./styles/partials/_resets.scss";
-import Navbar from "./components/Navbar/navbar";
 import Landing from "./components/Landing/landing";
 import PageTwo from "./components/PageTwo/pageTwo";
-import Mission from "./components/mission/mission";
+import Mission from "./components/Mission/mission";
+import Phone from "./components/Phone/phone";
 function App() {
   return (
     <div className="App">
       <Landing />
       <PageTwo />
       <Mission />
+      <Phone />
     </div>
   );
 }
---
+++ src/components/Phone/phone.jsx
@@ -0,0 +1,49 @@
+import "./phone.scss";
+const Phone = () => {
+  return (
+    <section className="phone">
+      <div className="phone__bg">
+        <div className="phone__text">
+          <div className="phone__text-container">
+            <h3 className="phone__subheader">Head</h3>
+            <p className="phone__text-p">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Laudantium officia cum quibusdam maxime accusamus ducimus harum
+              pariatur praesentium dolorem. Dolorem.
+            </p>
+          </div>
+
+          <div className="phone__text-container">
+            <h3 className="phone__subheader">Head</h3>
+            <p className="phone__text-p">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Laudantium officia cum quibusdam maxime accusamus ducimus harum
+              pariatur praesentium dolorem. Dolorem.
+            </p>
+          </div>
+        </div>
+        <div className="phone__phone"></div>
+        <div className="phone__text">
+          <div className="phone__text-container">
+            <h3 className="phone__subheader">Head</h3>
+            <p className="phone__text-p">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Laudantium officia cum quibusdam maxime accusamus ducimus harum
+              pariatur praesentium dolorem. Dolorem.
+            </p>
+          </div>
+          <div className="phone__text-container">
+            <h3 className="phone__subheader">Head</h3>
+            <p className="phone__text-p">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Laudantium officia cum quibusdam maxime accusamus ducimus harum
+              pariatur praesentium dolorem. Dolorem.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Phone;
