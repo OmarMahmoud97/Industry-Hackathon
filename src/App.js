ea7610e2472ec1457e5f856173b0c89323e37393
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2022-10-27 21:42:36 +0100

    add mission section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./styles/partials/_resets.scss";
 import Navbar from "./components/Navbar/navbar";
 import Landing from "./components/Landing/landing";
 import PageTwo from "./components/PageTwo/pageTwo";
+import Mission from "./components/mission/mission";
 function App() {
   return (
     <div className="App">
       <Landing />
       <PageTwo />
+      <Mission />
     </div>
   );
 }
