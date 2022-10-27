49fa212dd8bdf1707175ce4273b8accf2986225a
Rolphy Lu Morrell <rlumorrell19@gmail.com>
2022-10-27 21:48:33 +0100

    footer structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles/partials/_resets.scss";
 import Navbar from "./components/Navbar/navbar";
 import Landing from "./components/Landing/landing";
+import Footer from "./components/Footer/footer";
 function App() {
   return (
     <div className="App">
       <Landing />
+      <Footer />
     </div>
   );
 }
